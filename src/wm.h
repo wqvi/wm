434307c814eeaf6400130417157e3b83c146d551
mynahisnowhere <137255931+mynahisnowhere@users.noreply.github.com>
2023-07-16 17:37:01 -0700

    Moved structs out of dwl.c to wm.h
    This is to make the code base much easier to manage. Contrary to what
    Plan9 thinks that just clutters and makes it harder to read.

--- src/wm.h
+++ src/wm.h
@@ -53,4 +53,93 @@ typedef struct {
 	uint32_t resize; // configure serial of a pending size
 } Client;
 
+typedef struct {
+	struct wl_list link;
+	struct wlr_keyboard *wlr_keyboard;
+
+	int nsyms;
+	const xkb_keysym_t *keysyms; // invalid if nsyms == 0
+	uint32_t mods; // invalid if nsyms == 0
+	struct wl_event_source *key_repeat_source;
+
+	struct wl_listener modifiers;
+	struct wl_listener key;
+	struct wl_listener destroy;
+} Keyboard;
+
+typedef struct {
+	// Must keep these three elements in this order
+	unsigned int type; // layer shell
+	struct wlr_box geom;
+	Monitor *mon;
+	struct wlr_scene_tree *scene;
+	struct wlr_scene_tree *popups;
+	struct wlr_scene_layer_surface_v1 *scene_layer;
+	struct wl_list link;
+	int mapped;
+	struct wlr_layer_surface_v1 *layer_surface;
+
+	struct wl_listener destroy;
+	struct wl_listener map;
+	struct wl_listener unmap;
+	struct wl_listener surface_commit;
+} LayerSurface;
+
+typedef struct {
+	const char *symbol;
+	void (*arrange)(Monitor *);
+} Layout;
+
+struct Monitor {
+	struct wl_list link;
+	struct wlr_output *wlr_output;
+	struct wlr_scene_output *scene_output;
+	struct wlr_scene_rect *fullscreen_bg; // See createmon() for info
+	struct wl_listener frame;
+	struct wl_listener destroy;
+	struct wl_listener destroy_lock_surface;
+	struct wlr_session_lock_surface_v1 *lock_surface;
+	struct wlr_box m; // monitor area, layout-relative
+	struct wlr_box w; // window area, layout-relative
+	struct wl_list layers[4]; // LayerSurface::link
+	const Layout *lt[2];
+	unsigned int seltags;
+	unsigned int sellt;
+	uint32_t tagset[2];
+	double mfact;
+	int nmaster;
+	char ltsymbol[16];
+};
+
+typedef struct {
+	const char *name;
+	float mfact;
+	int nmaster;
+	float scale;
+	const Layout *lt;
+	enum wl_output_transform rr;
+	int x, y;
+} MonitorRule;
+
+typedef struct {
+	const char *id;
+	const char *title;
+	uint32_t tags;
+	int is_floating;
+	int monitor;
+} Rule;
+
+typedef struct {
+	struct wlr_scene_tree *scene;
+
+	struct wlr_session_lock_v1 *lock;
+	struct wl_listener new_surface;
+	struct wl_listener unlock;
+	struct wl_listener destroy;
+} SessionLock;
+
+enum { CurNormal, CurPressed, CurMove, CurResize }; // cursor
+enum { XDGShell, LayerShell }; // client types
+enum { LyrBg, LyrBottom, LyrTile, LyrFloat, LyrFS, LyrTop, LyrOverlay, LyrBlock, NUM_LAYERS }; // scene layers
+
 #endif // WM_H
